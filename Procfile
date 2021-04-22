d259b1e08b8974f6cc57e0ef2e03609a5b665cdd
Pierrelean @bolshoyshlyoppa aka Leonid Perlin Vadimovich <perlinleo@gmail.com>
2021-04-22 15:39:49 +0300

    Procfile added

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn askme.wsgi --log-file -
--- askme/settings.py
+++ askme/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = '/var/www/example.com/static/'
+
 STATICFILES_DIRS = [
     'static',
     '/var/www/static/',
