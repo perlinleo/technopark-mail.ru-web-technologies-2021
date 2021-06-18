e29977d1cd1b54e8337a9a7b2d1282c7faac9c05
Pierrelean @bolshoyshlyoppa aka Leonid Perlin Vadimovich <perlinleo@gmail.com>
2021-06-18 12:17:25 +0300

    script

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@ python3 -m venv venv
 cd venv
 source bin/activate
 cd .. 
+pip install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py filldatabase --full 10
